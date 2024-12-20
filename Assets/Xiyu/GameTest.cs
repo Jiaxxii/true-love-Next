cbc457e86cfcda008abda12b20d788d30d3a9037
xiyu <jiaxxii@163.com>
2024-12-20 17:31:28 +0800

    first commit

---
+++ Assets/Xiyu/GameTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using Xiyu.StandingIllustration;
+
+
+namespace MyNamespace
+{
+    public class GameTest : MonoBehaviour
+    {
+        private async void Start()
+        {
+            var standingIllustrationLoader = new StandingIllustrationLoader();
+            await standingIllustrationLoader.LoadStandingIllustrationAsync("真奈美a_0_1196");
+        }
+    }
+}
---
+++ Assets/Xiyu/StandingIllustration/StandingIllustrationLoader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Xiyu.StandingIllustration
+{
+    public class StandingIllustrationLoader
+    {
+        private readonly ConcurrentDictionary<string, AsyncOperationHandle<Sprite>> _bufferMap = new();
+
+        public async UniTask<Sprite> LoadStandingIllustrationAsync(string addressableName)
+        {
+            _bufferMap.TryAdd(addressableName, Addressables.LoadAssetAsync<Sprite>(addressableName));
+
+
+            var asyncOperationHandle = _bufferMap[addressableName];
+            Debug.Log(asyncOperationHandle.Status);
+            if (!asyncOperationHandle.IsDone)
+            {
+                await UniTask.NextFrame();
+            }
+
+            if (asyncOperationHandle.Status != AsyncOperationStatus.Succeeded)
+            {
+                throw new ArgumentException("资源加载失败！状态：" + asyncOperationHandle.Status);
+            }
+
+            return asyncOperationHandle.Result;
+        }
+    }
+}
